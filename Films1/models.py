986cbc3c204524049c8548185a3bbdcf9ae6f46e
Камиль Марданшин <kmardanshin2023@LI2.local>
2022-04-29 16:28:36 +0300

    added models

--- Films1/models.py
+++ Films1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class film(models.Model):
+    release_date=models.DateField()
+    film_name=models.CharField(max_length=50)
+    description=models.CharField(max_length=255)
+class actor(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    birthday=models.DateField()
+    film=models.ManyToManyField(to=film)
+class director(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    birthday=models.DateField()
+    film=models.ManyToManyField(to=film)
